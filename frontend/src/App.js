905348a8ce1a3cc6a3fa1a9b1230b4e46e64ee00
CesarOLIVEROS <cesaroliveros23@gmail.com>
2022-04-17 22:45:12 -0500

    Create npx react native and run App
    I am doing run App whitn npm run start and create the first components in JSX and organized folder.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ButtonOk from "./components/ButtonOK";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <React.Fragment>
+
+    <h1>Estudiantes, MINTIC!</h1>
+  
+    <h2>Grupo de desarrollo</h2>
+    <ButtonOk/>
+  </React.Fragment>
   );
 }
 
---
+++ frontend/src/components/ButtonOK.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonOk = () => {
+  return (
+    <>
+      <button>Enviar</button>
+    </>
+  )
+}
+
+export default ButtonOk
