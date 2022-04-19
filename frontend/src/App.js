49c67f77df9307a081bf8c0aa3dd968472a0c124
CesarOLIVEROS <cesaroliveros23@gmail.com>
2022-04-18 22:48:34 -0500

    All install bootstrap and create Route to React
    i am create routes, and be ready for use in the frontend, besides installing react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Landing from "./screens/Landing";
