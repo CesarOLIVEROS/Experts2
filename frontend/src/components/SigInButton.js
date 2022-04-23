c7cdca230224fcdf5042ec2b167edb4032a9646e
CesarOLIVEROS <cesaroliveros23@gmail.com>
2022-04-23 01:00:14 -0500

    Version whitn acces for google and facebook granted

--- frontend/src/components/SigInButton.js
+++ frontend/src/components/SigInButton.js
@@ -1,16 +1,47 @@
+// Import FirebaseAuth and firebase.
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-const SigInButton = () => {
-    return (
-        <>
-            
-                <Button variant="dark" size="lg" style={{borderRadius: 13, backgroundColor: 'black'}}>
-                    Login
-                </Button>
-            
-        </>
-    )
+// Configure Firebase.
+const config = {
+    apiKey: "AIzaSyCELfD0zb0v2LtwMD0NaoK7U21aQrsoOdA",
+
+    authDomain: "pruebaweb1-c24e7.firebaseapp.com",
+  
+    projectId: "pruebaweb1-c24e7",
+  
+    storageBucket: "pruebaweb1-c24e7.appspot.com",
+  
+    messagingSenderId: "878317647478",
+  
+    appId: "1:878317647478:web:7898c04439ba425e7a9157"
+  
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/home',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function SignInButton() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
 }
 
-export default SigInButton
+export default SignInButton
